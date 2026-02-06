24fed41b5e51169648ad71a8894c28cde2b1d24e
nstechbytes <nstechbytes@gmail.com>
2026-02-06 14:56:22 +0500

    feat: migrate project to Docusaurus, update dependencies, and restructure documentation

---
+++ src/theme/MDXComponents.tsx
@@ -0,0 +1,8 @@
+import type {MDXComponents} from '@mdx-js/react';
+import FileTree from '@site/src/components/FileTree';
+
+const components: MDXComponents = {
+  FileTree,
+};
+
+export default components;
