00a26c96996e099105dfb5d97efa7fd42576c8af
nstechbytes <nstechbytes@gmail.com>
2026-02-06 17:25:01 +0500

    feat: add Badge component and integrate with sidebar items

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -1,8 +1,10 @@
 import type {MDXComponents} from '@mdx-js/react';
 import FileTree from '@site/src/components/FileTree';
+import Badge from '@site/src/components/Badge';
 
 const components: MDXComponents = {
   FileTree,
+  Badge,
 };
 
 export default components;
