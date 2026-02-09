3c596c4041e6d17f1e8606fc90ed64f92df99c5a
nstechbytes <nstechbytes@gmail.com>
2026-02-09 13:53:55 +0500

    add back-to-top button component in theme.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,6 +5,8 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import { LiteTree } from '@lite-tree/vue'
+import BackToTopButton from '@miletorix/vitepress-back-to-top-button' 
+import '@miletorix/vitepress-back-to-top-button/style.css'
 
 export default {
   extends: DefaultTheme,
@@ -16,5 +18,8 @@ export default {
   enhanceApp({ app, router, siteData }) {
     enhanceAppWithTabs(app)
     app.component('LiteTree', LiteTree)
+    BackToTopButton(app, {
+      progressColor: '#3f4152', 
+    }) //[!code ++]
   }
 } satisfies Theme
